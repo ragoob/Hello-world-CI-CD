ebda422eb632938210b4993c1ddd1935f5bdc2b9
$AMLRT-app <Mohamed.ragab@integrant.com>
2020-06-11 01:45:23 +0200

    change app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Git/Lab CI/CD';
+    return 'Hello Git/Lab CI/CD against Rancher';
   }
 }
